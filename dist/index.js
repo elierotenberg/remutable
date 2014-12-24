38607ad1109b2c9b92d3e55cad710c4dd329614a
Elie Rotenberg <elie.rotenberg@gmail.com>
2014-12-24 14:57:49 +0100

    Initial commit.

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,111 @@
 "use strict";
 
-require("6to5/polyfill");var Promise = (global || window).Promise = require("lodash-next").Promise;var __DEV__ = (process.env.NODE_ENV !== "production");var __PROD__ = !__DEV__;var __BROWSER__ = (typeof window === "object");var __NODE__ = !__BROWSER__;
+require("6to5/polyfill");var Promise = (global || window).Promise = require("lodash-next").Promise;var __DEV__ = (process.env.NODE_ENV !== "production");var __PROD__ = !__DEV__;var __BROWSER__ = (typeof window === "object");var __NODE__ = !__BROWSER__;var _ = require("lodash-next");
+
+var MUTATIONS = {
+  SET: "s",
+  DEL: "d" };
+
+var Remutable = (function () {
+  var Remutable = function Remutable() {
+    this._data = {};
+    this._mutations = {};
+    this._version = 0;
+    this._remutableId = _.random(0, 4294967296);
+  };
+
+  Remutable.prototype.get = function (key) {
+    if (this._mutations[key] !== void 0) {
+      var m = this._mutations[key].m;
+      var v = this._mutations[key].v;
+      if (m === MUTATIONS.DEL) {
+        return void 0;
+      }
+      if (m === MUTATIONS.SET) {
+        return v;
+      }
+    }
+    return this._data[key];
+  };
+
+  Remutable.prototype.set = function (key, val) {
+    this._mutations[key] = { d: MUTATIONS.SET, v: val };
+  };
+
+  Remutable.prototype.del = function (key) {
+    this._mutations[key] = { d: MUTATIONS.DEL };
+  };
+
+  Remutable.prototype.commit = function () {
+    return this._applyPatchWithoutCheckingMutations({
+      remutableId: this._remutableId,
+      mutations: this._mutations,
+      prev: this._version,
+      next: this._version + 1 });
+  };
+
+  Remutable.prototype.rollback = function () {
+    this._mutations = {};
+  };
+
+  Remutable.prototype._applyPatchWithoutCheckingMutations = function (patch) {
+    var _this = this;
+    var remutableId = patch.remutableId;
+    var mutations = patch.mutations;
+    var prev = patch.prev;
+    var next = patch.next;
+    this._remutableId.should.be.exactly(remutableId);
+    this._version.should.be.exactly(prev);
+    Object.keys(mutations).forEach(function (key) {
+      var m = _this._mutations[key].m;
+      var v = _this._mutations[key].v;
+      if (m === MUTATIONS.DEL) {
+        delete _this._data[key];
+      }
+      if (m === MUTATIONS.SET) {
+        _this._data[key] = v;
+      }
+    });
+    this._version = next;
+    this._mutations = {};
+    return patch;
+  };
+
+  Remutable.prototype.apply = function (patch) {
+    this._mutations.should.eql({});
+    return this._applyPatchWithoutCheckingMutations(patch);
+  };
+
+  Remutable.prototype.serialize = function () {
+    return JSON.stringify({
+      remutableId: this._remutableId,
+      version: this._version,
+      data: this._data });
+  };
+
+  Remutable.unserialize = function (serialized) {
+    var _ref = JSON.parse(serialized);
+
+    var remutableId = _ref.remutableId;
+    var version = _ref.version;
+    var data = _ref.data;
+    _.dev(function () {
+      return remutableId.should.be.a.Number && version.should.be.a.Number && data.should.be.an.Object;
+    });
+    var remutable = new Remutable();
+    remutable._remutableId = remutableId;
+    remutable._version = version;
+    remutable._data = data;
+    return remutable;
+  };
+
+  return Remutable;
+})();
+
+_.extend(Remutable.prototype, {
+  _data: null,
+  _mutations: null,
+  _version: null,
+  _remutableId: null });
+
+module.exports = Remutable;
---
+++ src/index.js
@@ -0,0 +1,102 @@
+const _ = require('lodash-next');
+
+const MUTATIONS = {
+  SET: 's',
+  DEL: 'd',
+};
+
+class Remutable {
+  constructor() {
+    this._data = {};
+    this._mutations = {};
+    this._version = 0;
+    this._remutableId = _.random(0, 4294967296);
+  }
+
+  get(key) {
+    if(this._mutations[key] !== void 0) {
+      const { m, v } = this._mutations[key];
+      if(m === MUTATIONS.DEL) {
+        return void 0;
+      }
+      if(m === MUTATIONS.SET) {
+        return v;
+      }
+    }
+    return this._data[key];
+  }
+
+  set(key, val) {
+    this._mutations[key] = { d: MUTATIONS.SET, v: val };
+  }
+
+  del(key) {
+    this._mutations[key] = { d: MUTATIONS.DEL };
+  }
+
+  commit() {
+    return this._applyPatchWithoutCheckingMutations({
+      remutableId: this._remutableId,
+      mutations: this._mutations,
+      prev: this._version,
+      next: this._version + 1,
+    });
+  }
+
+  rollback() {
+    this._mutations = {};
+  }
+
+  _applyPatchWithoutCheckingMutations(patch) {
+    const { remutableId, mutations, prev, next } = patch;
+    this._remutableId.should.be.exactly(remutableId);
+    this._version.should.be.exactly(prev);
+    Object.keys(mutations).forEach((key) => {
+      const { m, v } = this._mutations[key];
+      if(m === MUTATIONS.DEL) {
+        delete this._data[key];
+      }
+      if(m === MUTATIONS.SET) {
+        this._data[key] = v;
+      }
+    });
+    this._version = next;
+    this._mutations = {};
+    return patch;
+  }
+
+  apply(patch) {
+    this._mutations.should.eql({});
+    return this._applyPatchWithoutCheckingMutations(patch);
+  }
+
+  serialize() {
+    return JSON.stringify({
+      remutableId: this._remutableId,
+      version: this._version,
+      data: this._data,
+    });
+  }
+
+  static unserialize(serialized) {
+    const { remutableId, version, data } = JSON.parse(serialized);
+    _.dev(() => remutableId.should.be.a.Number &&
+      version.should.be.a.Number &&
+      data.should.be.an.Object
+    );
+    const remutable = new Remutable();
+    remutable._remutableId = remutableId;
+    remutable._version = version;
+    remutable._data = data;
+    return remutable;
+  }
+}
+
+_.extend(Remutable.prototype, {
+  _data: null,
+  _mutations: null,
+  _version: null,
+  _remutableId: null,
+});
+
+module.exports = Remutable;
